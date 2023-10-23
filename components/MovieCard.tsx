b46884a6db0bde53acca22d688a36b088c83f9b8
maleeshagimmana <it21176456@my.sliit.lk>
2023-10-23 15:19:08 +0530

    maleesha edit

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,35 +1,43 @@
-import useInforModal from '@/hooks/useInfoModal';
-import React from 'react';
+import React, { useState } from 'react';
 import { CgDetailsMore } from 'react-icons/cg';
+// import '../styles/MovieCard.css';
 
 interface MovieCardProps {
-    data: Record<string, any>;
+  data: Record<string, any>;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({
-    data
-}) => {
-    const cardStyle = {
-        backgroundColor: '#414654',
-    };
+const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const { openModal } = useInforModal();
+  const cardStyle = {
+    backgroundColor: '#414654',
+    height: isExpanded ? 'auto' : '12vw',
+  };
 
-    //const handleClick = () => {
-        // Simulate navigation to the new page with the movie details
-        //console.log(`Navigating to movie details for ID: ${data.id}`);
-    //};
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
 
-    return (
-        <div className='group'>
-            <div className='bg-zinc-900 text-xl font-semibold col-span relative h-[12vw] rounded-lg p-4 hover:shadow-lg transition duration-200' style={cardStyle}>
-                <p className='text-white'>{data.title}</p>
-                <div className='absolute bottom-5 left-5 text-white' onClick={() => openModal (data?.id)}>
-                    <CgDetailsMore size={30} />
-                </div>
-            </div>
+  return (
+    <div className="group">
+      <div
+        className="bg-zinc-900 text-xl font-semibold col-span relative rounded-lg p-4 hover:shadow-lg transition duration-200"
+        style={cardStyle}
+      >
+        <p className="text-white">{data.title}</p>
+        {isExpanded && (
+          <div className="details">
+
+            <p>{data.overview}</p>
+            {/* Add additional movie details here */}
+          </div>
+        )}
+        <div className="expand-icon" onClick={toggleExpand}>
+          <CgDetailsMore size={30} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
