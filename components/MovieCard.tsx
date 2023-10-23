ddb3a3b225d7e5cfc95e577d80f652a0fd4f4772
maleeshagimmana <it21176456@my.sliit.lk>
2023-10-23 16:04:24 +0530

    card is done

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,41 +1,67 @@
 import React, { useState } from 'react';
 import { CgDetailsMore } from 'react-icons/cg';
-// import '../styles/MovieCard.css';
+import Modal from 'react-modal';
 
 interface MovieCardProps {
   data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const cardStyle = {
-    backgroundColor: '#414654',
-    height: isExpanded ? 'auto' : '12vw',
+  const openModal = () => {
+    setIsModalOpen(true);
   };
 
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
     <div className="group">
       <div
-        className="bg-zinc-900 text-xl font-semibold col-span relative rounded-lg p-4 hover:shadow-lg transition duration-200"
-        style={cardStyle}
+        className='bg-zinc-900 text-xl font-semibold col-span relative h-[12vw] rounded-lg p-4 hover:shadow-lg transition duration-200' 
+        style={{ backgroundColor: '#414654'}}
       >
         <p className="text-white">{data.title}</p>
-        {isExpanded && (
-          <div className="details">
-
-            <p>{data.overview}</p>
-            {/* Add additional movie details here */}
-          </div>
-        )}
-        <div className="expand-icon" onClick={toggleExpand}>
+        <div className="expand-icon" onClick={openModal}>
           <CgDetailsMore size={30} />
         </div>
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          },
+          content: {
+            backgroundColor: '#575757',
+            color: '#ffffff',
+            border: 'none',
+            borderRadius: '8px',
+            padding: '20px',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '400px',
+            maxHeight: '80vh',
+            overflow: 'auto',
+          },
+        }}
+      >
+        <div className="modal-content">
+          <h2 style={{fontWeight:'bold'}}>{data.title}</h2>
+          <br />
+
+          <p>{data.overview}</p>
+          {/* Add additional movie details here */}
+            <p>Release Date: {data.release_date}</p>
+
+            <br />
+          <button onClick={closeModal}>Close</button>
+        </div>
+      </Modal>
     </div>
   );
 };
